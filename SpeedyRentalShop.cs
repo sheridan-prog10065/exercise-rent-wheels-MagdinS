e751a607d26b8c26c84c924207b8df920d06b3c7
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-02-14 10:18:18 -0500

    Define the speedy rental shop class.

--- SpeedyRentalShop.cs
+++ SpeedyRentalShop.cs
@@ -1,6 +1,31 @@
+using System.Collections.ObjectModel;
+
 namespace RentWheelsApp;
 
 public class SpeedyRentalShop
-{ 
+{
+	private ObservableCollection<Rental> _rentalCollection;
+	private ObservableCollection<Vehicle> _vehicleCollection;
 	
+	public SpeedyRentalShop()
+	{
+		_rentalCollection = new ObservableCollection<Rental>();
+		_vehicleCollection = new ObservableCollection<Vehicle>();
+	}
+
+	public ObservableCollection<Rental> Rentals
+	{
+		get
+		{
+			return _rentalCollection;
+		}
+	}
+
+	public ObservableCollection<Vehicle> Vehicles
+	{
+		get
+		{
+			return _vehicleCollection;
+		}
+	}
 }
