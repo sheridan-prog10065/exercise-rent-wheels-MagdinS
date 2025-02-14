b2248a53ba4e15c84e53f38d0364e87c0583d644
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-02-14 10:19:38 -0500

    Display the list of vehicles in the rental page.

--- RentalPage.xaml.cs
+++ RentalPage.xaml.cs
@@ -3,6 +3,7 @@
 public partial class RentalPage : ContentPage
 {
     private SpeedyRentalShop _rentalShop;
+    
     public RentalPage(SpeedyRentalShop rentalShop)
     {
         //initialize the business logic service before any controls are created
@@ -10,5 +11,8 @@ public partial class RentalPage : ContentPage
         
         InitializeComponent();
         
+        //link the vehicle inventory view with the list of vehicles
+        _lstVehicleInventory.ItemsSource = _rentalShop.Vehicles;
+
     }
 }
