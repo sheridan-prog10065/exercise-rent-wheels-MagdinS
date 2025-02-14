e135abf082a05612739157659ede810372b16494
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-02-14 14:19:54 -0500

    Create three default vehicles

--- SpeedyRentalShop.cs
+++ SpeedyRentalShop.cs
@@ -11,6 +11,33 @@ public class SpeedyRentalShop
 	{
 		_rentalCollection = new ObservableCollection<Rental>();
 		_vehicleCollection = new ObservableCollection<Vehicle>();
+		
+		//load default vehicles
+		CreateDefaultVehicles();
+	}
+
+	private void CreateDefaultVehicles()
+	{
+		//create first vehicle and add it to the list
+		Vehicle vehicle1 = new Vehicle("Honda");
+		vehicle1.Mileage = 10000;
+		vehicle1.LicensePlate = "ABD 3456";
+		vehicle1.PassengerCapacity = 5;
+		_vehicleCollection.Add(vehicle1);
+
+		//create second vehicle and add it to the list
+		Vehicle vehicle2 = new Vehicle("Toyota");
+		vehicle2.Mileage = 900;
+		vehicle2.LicensePlate = "FGH 1204";
+		vehicle2.PassengerCapacity = 7;
+		_vehicleCollection.Add(vehicle2);		
+
+		//create third vehicle and add it to the list
+		Vehicle vehicle3 = new Vehicle("Chevy");
+		vehicle3.Mileage = 75000;
+		vehicle3.LicensePlate = "XYZ 3688";
+		vehicle3.PassengerCapacity = 6;
+		_vehicleCollection.Add(vehicle3);		
 	}
 
 	public ObservableCollection<Rental> Rentals
