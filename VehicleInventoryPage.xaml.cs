ab7ecf1f4d5b463f8b8bd6042299d136283829d5
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-02-14 10:19:14 -0500

    Implement adding a vehicle.

--- VehicleInventoryPage.xaml.cs
+++ VehicleInventoryPage.xaml.cs
@@ -10,5 +10,33 @@ public partial class VehicleInventoryPage : ContentPage
 		_rentalShop = rentalShop;
 		
 		InitializeComponent();
+		
+		//connect the collection view with the list of vehicles
+		_lstVehicleInventory.ItemsSource = _rentalShop.Vehicles;
+	}
+
+	private void OnAddVehicle(object sender, EventArgs e)
+	{
+		//Read the vehicle properties
+		string vehicleMake = _txtMake.Text;
+		byte passCapacity = byte.Parse(_txtPassCapacity.Text);
+		double mileage = double.Parse(_txtMileage.Text);
+		string licensePlate = _txtLicensePlate.Text;
+
+		//Create a vehicle object
+		Vehicle newVehicle = new Vehicle(vehicleMake);
+		
+		//Set the vehicle properties to the user input
+		newVehicle.PassengerCapacity = passCapacity;
+		newVehicle.Mileage = mileage;
+		newVehicle.LicensePlate = licensePlate;
+		
+		//Add the vehicle to the vehicle inventory of the rental shop
+		_rentalShop.Vehicles.Add(newVehicle);
+	}
+
+	private void OnClear(object sender, EventArgs e)
+	{
+		//TODO: implement clearing the form
 	}
 }
